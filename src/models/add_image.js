ae0a24ab837fb07dfc66bf5190fc89a27d31c1a9
Shivanshu Gupta <shivanshu.gupta@gmail.com>
2020-03-07 08:25:11 -0800

    added more fields to image data

--- src/models/add_image.js
+++ src/models/add_image.js
@@ -1,21 +1,45 @@
 const mongoose = require("mongoose");
 const uniqid = require("uniqid");
+var rn = require('random-number');
 
 const AddImageSchema = new mongoose.Schema({
   handle: {
     type: String,
     alias: "user's handle",
-    default: uniqid(),
+    default: "",
+    required: true,
   },
   title: {
     type: String,
     alias: "user's image title",
-    default: uniqid(),
+    default: "",
+    required: true,
   },
   url: {
     type: String,
     alias: "user's image url",
+    default: "",
+    required: true,
+  },
+  votes: {
+    type: Number,
+    alias: "user's post value",
+    default: rn({min:  0, max:  1000, integer: true}),
+  },
+  viewerCount: {
+    type: Number,
+    alias: "user viewer value",
+    default: rn({min:  0, max:  50000, integer: true}),
+  },
+  postID: {
+    type: String,
+    alias: "user's post ID",
     default: uniqid(),
+  },
+  comments: {
+    type: Array,
+    alias: "user's post comments",
+    default: [],
   }
 });
 
